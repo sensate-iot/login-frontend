3e71f009d90e8263f94106275ac80a89f26a7ed5
Michel Megens <michel@michelmegens.net>
2020-10-07 18:51:32 +0200

    app: disable debug logging in production
    Disable `console.debug()' output in production builds.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
+function main() {
+  if (environment.production) {
+    enableProdMode();
+    console.debug = function () { };
+  }
+
+  platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(err => console.error(err));
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+main();
